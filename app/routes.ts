07cd5295defab7009e0236188644b2499392a402
Steven Foote <steven@resonance.run>
2025-11-07 15:44:13 -0700

    Add some course routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,8 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from '@react-router/dev/routes';
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index('routes/home.tsx'),
+  route('course', 'routes/course/list.tsx'),
+  route('course/new', 'routes/course/new.tsx'),
+  route('course/:id', 'routes/course/detail.tsx'),
+] satisfies RouteConfig;
